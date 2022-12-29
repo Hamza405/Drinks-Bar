853892bca042bc74d9cd0578e281fc4307d0a6b6
Hamza405 <hamzash863@gmail.com>
2022-12-29 23:54:11 +0200

    Implement rest api methods

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import {
+  getAlcoholicTypesRequest,
+  getCategoriesRequest,
+  getGlassesRequest,
+  getIngredientsRequest,
+} from "./api/CocktailsApi";
 import "./App.css";
 import Cocktails from "./components/Cocktails";
 import Header from "./components/Header";
--- src/api/CocktailsApi.js
+++ src/api/CocktailsApi.js
@@ -12,3 +12,55 @@ export async function getCocktailsRequest() {
 
   return data.drinks;
 }
+
+export async function getIngredientsRequest() {
+  const res = await fetch(`${endpoint}list.php?i=list`);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    console.log(data);
+    throw new Error(data.error.message || "Could not login!.");
+  }
+
+  return data.drinks;
+}
+
+export async function getGlassesRequest() {
+  const res = await fetch(`${endpoint}list.php?g=list`);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    console.log(data);
+    throw new Error(data.error.message || "Could not login!.");
+  }
+
+  return data.drinks;
+}
+
+export async function getCategoriesRequest() {
+  const res = await fetch(`${endpoint}list.php?c=list`);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    console.log(data);
+    throw new Error(data.error.message || "Could not login!.");
+  }
+
+  return data.drinks;
+}
+
+export async function getAlcoholicTypesRequest() {
+  const res = await fetch(`${endpoint}list.php?c=list`);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    console.log(data);
+    throw new Error(data.error.message || "Could not login!.");
+  }
+
+  console.log(data);
+}
